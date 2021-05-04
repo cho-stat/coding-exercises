4fb97db12c558afa097b0c1240b3ee04fe5fa2ba
Christine Ho <cho@pandora.com>
2021-05-03 23:52:54 -0300

    initial commit

---
+++ find_repeat.py
@@ -0,0 +1,46 @@
+import unittest
+
+
+def find_repeat(numbers):
+
+    # Find a number that appears more than once
+    number_dict = {}
+    for i in numbers: 
+        if i in number_dict: 
+            return i 
+        else: 
+            number_dict[i] = 1
+    
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_just_the_repeated_number(self):
+        actual = find_repeat([1, 1])
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_short_list(self):
+        actual = find_repeat([1, 2, 3, 2])
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_medium_list(self):
+        actual = find_repeat([1, 2, 5, 5, 5, 5])
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_long_list(self):
+        actual = find_repeat([4, 1, 4, 8, 3, 2, 7, 6, 5])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
